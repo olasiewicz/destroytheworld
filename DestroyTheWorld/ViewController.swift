dabb598ad98cf0cfb647069df6bd14abd24b0202
olasiewicz <wojtek@Mobicas-Mac-mini-5.local>
2017-04-14 09:13:36 +0200

    first commit

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hello tu Wojtas!!!")
+        print("commit test")
     }
 
     override func didReceiveMemoryWarning() {
